c043ee1c8a0a65aab1862d0e9e42501c12f66884
pl0xyelit <mihai.chiriac2005@yahoo.ro>
2022-10-29 17:22:20 +0300

    Initial commit

---
+++ cfone/Commands/MyFirstModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+
+public class MyFirstModule : BaseCommandModule
+{
+    [Command("greet")]
+    public async Task GreetCommand(CommandContext ctx, string username) 
+    {
+        await ctx.RespondAsync($"Greetings, {username}, from pl0xy's 2018 laptop");
+    }
+}
---
+++ cfone/Program.cs
@@ -0,0 +1,34 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+namespace cfone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "add your own bot token here or figure out a way to make a config.json lmfao",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "p!", "c!" }
+            });
+
+            commands.RegisterCommands<MyFirstModule>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
