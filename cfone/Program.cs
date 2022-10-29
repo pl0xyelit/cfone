6d1b8c46194bb70408c2a9d4f5b58899dec89a28
pl0xyelit <mihai.chiriac2005@yahoo.ro>
2022-10-29 20:38:36 +0300

    Added Lavalink-based music commands

--- cfone/Program.cs
+++ cfone/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Net;
+using DSharpPlus.Lavalink;
 namespace cfone
 {
     class Program
@@ -15,7 +17,7 @@ namespace cfone
         {
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "add your own bot token here or figure out a way to make a config.json lmfao",
+                Token = "NjkwOTk3MzA0OTg3MDkwOTQ0.G4Yiiz.XDxMFqOdgQ4gDYd9suVVPg8utXM6lVi0UvJ7UI",
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
@@ -26,8 +28,27 @@ namespace cfone
             });
 
             commands.RegisterCommands<MyFirstModule>();
+            commands.RegisterCommands<MusicCommands>();
+
+            var endpoint = new ConnectionEndpoint
+            {
+                Hostname = "127.0.0.1", // From your server configuration.
+                Port = 2333 // From your server configuration
+            };
+
+            var lavalinkConfig = new LavalinkConfiguration
+            {
+                Password = "youshallnotpass", // From your server configuration.
+                RestEndpoint = endpoint,
+                SocketEndpoint = endpoint
+            };
+
+            var lavalink = discord.UseLavalink();
 
             await discord.ConnectAsync();
+
+            await lavalink.ConnectAsync(lavalinkConfig);
+
             await Task.Delay(-1);
         }
     }
